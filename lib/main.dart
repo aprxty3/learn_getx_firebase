70cbeb80d71d6bcf7c48193ace6009d46761d580
aprxty3 <ajiprxty3@gmail.com>
2022-03-22 14:28:35 +0700

    fixing integrated with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'gett';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
