a3492b36a37bd9f32c38e933da344dbc0df71302
aprxty3 <ajiprxty3@gmail.com>
2022-03-23 15:23:55 +0700

    redesign the wireframe

--- getx_firebase3_reset_password/lib/app/modules/reset_password/views/reset_password_view.dart
+++ getx_firebase3_reset_password/lib/app/modules/reset_password/views/reset_password_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:learn_getx_firebase/app/controllers/auth_controller.dart';
+import 'package:learn_getx_firebase/app/routes/app_pages.dart';
 
 import '../controllers/reset_password_controller.dart';
 
@@ -11,37 +12,59 @@ class ResetPasswordView extends GetView<ResetPasswordController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('ResetPasswordView'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text('ResetPasswordView'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: controller.emailC,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              // controller: controller.passwordC,
+              decoration: InputDecoration(labelText: "Password"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                authC.resetPassword(controller.emailC.text);
+              },
+              child: Text('Reset'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'Sudah Punya Akun?',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => Get.toNamed(Routes.LOGIN),
+                  child: Text(
+                    'Login!',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: controller.emailC,
-                decoration: InputDecoration(labelText: "Email"),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: controller.passwordC,
-                decoration: InputDecoration(labelText: "Password"),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  authC.login(
-                      controller.emailC.text, controller.passwordC.text);
-                },
-                child: Text('LOGIN'),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
