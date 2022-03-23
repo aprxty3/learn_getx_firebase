ff9f13ffb557f5afc89aa9b827218bf0c9088cc3
aprxty3 <ajiprxty3@gmail.com>
2022-03-23 15:23:38 +0700

    configure controller

--- getx_firebase3_reset_password/lib/app/modules/reset_password/controllers/reset_password_controller.dart
+++ getx_firebase3_reset_password/lib/app/modules/reset_password/controllers/reset_password_controller.dart
@@ -3,12 +3,10 @@ import 'package:get/get.dart';
 
 class ResetPasswordController extends GetxController {
   final emailC = TextEditingController(text: "ajicool26@gmail.com");
-  final passwordC = TextEditingController(text: "123123");
 
   @override
   void onClose() {
     emailC.dispose();
-    passwordC.dispose();
 
     super.onClose();
   }
