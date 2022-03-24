e8232d0375cd6a8571dc546e0ceae9e17c47b844
aprxty3 <ajiprxty3@gmail.com>
2022-03-24 16:37:58 +0700

    implement Stream snapshot

--- getx_firebase4_cloud_firestore/lib/app/modules/home/views/home_view.dart
+++ getx_firebase4_cloud_firestore/lib/app/modules/home/views/home_view.dart
@@ -25,10 +25,35 @@ class HomeView extends GetView<HomeController> {
           ),
         ],
       ),
-      body: FutureBuilder<QuerySnapshot<Object?>>(
-        future: controller.getData(),
+      // ONE TIME READ
+
+      // body: FutureBuilder<QuerySnapshot<Object?>>(
+      //   future: controller.getData(),
+      // builder: (context, snapshot) {
+      //   if (snapshot.connectionState == ConnectionState.done) {
+      //     var listAllDocs = snapshot.data!.docs;
+      //     return ListView.builder(
+      //       itemCount: listAllDocs.length,
+      //       itemBuilder: (context, index) => ListTile(
+      //         title: Text(
+      //             "${(listAllDocs[index].data() as Map<String, dynamic>)["nama"]}"),
+      //         subtitle: Text(
+      //             "Rp. ${(listAllDocs[index].data() as Map<String, dynamic>)["harga"]}"),
+      //       ),
+      //     );
+      //   }
+      //   return Center(
+      //     child: CircularProgressIndicator(),
+      //   );
+      // },
+      // ),
+
+      //REALTIME READ
+
+      body: StreamBuilder<QuerySnapshot<Object?>>(
+        stream: controller.streamData(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.connectionState == ConnectionState.active) {
             var listAllDocs = snapshot.data!.docs;
             return ListView.builder(
               itemCount: listAllDocs.length,
