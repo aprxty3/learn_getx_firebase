a93e1f76a28de8146cde545c7a8e1bae8e41a2b8
aprxty3 <ajiprxty3@gmail.com>
2022-03-24 23:15:33 +0700

    first commit

---
+++ getx_firebase5_query_data/README.md
@@ -0,0 +1,5 @@
+Learn About Cloud Firestore :
+- add Data
+- edit Data
+- delete Data
+- read Data (one time & stream)
---
+++ getx_firebase5_query_data/lib/app/modules/product/controllers/product_controller.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductController extends GetxController {
+  late TextEditingController nameC;
+  late TextEditingController priceC;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void addProduct(String name, String price) async {
+    CollectionReference products = firestore.collection("products");
+
+    try {
+      String dateNow = DateTime.now().toIso8601String();
+      await products.add(
+        {
+          "nama": name,
+          "harga": price,
+          "time": dateNow,
+        },
+      );
+
+      Get.defaultDialog(
+        title: "Berhasil",
+        middleText: "Berhasil menambahkan produk",
+        onConfirm: () {
+          nameC.clear();
+          priceC.clear();
+          Get.back();
+          Get.back();
+        },
+        textConfirm: "Ok",
+      );
+    } catch (e) {
+      print(e);
+      Get.defaultDialog(
+        title: "Terjadi kesalahan",
+        middleText: "Tidak berhasil menambahkan produk!",
+      );
+    }
+  }
+
+  @override
+  void onInit() {
+    nameC = TextEditingController();
+    priceC = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    nameC.dispose();
+    priceC.dispose();
+    super.onClose();
+  }
+}
