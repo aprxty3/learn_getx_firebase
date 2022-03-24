c4f65b0a23e5689824ae72666a10f5ae78fd8f2b
aprxty3 <ajiprxty3@gmail.com>
2022-03-24 15:17:41 +0700

    create data tiles & add FAB

--- getx_firebase4_cloud_firestore/lib/app/modules/home/views/home_view.dart
+++ getx_firebase4_cloud_firestore/lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:learn_getx_firebase/app/controllers/auth_controller.dart';
+import 'package:learn_getx_firebase/app/routes/app_pages.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -23,10 +24,17 @@ class HomeView extends GetView<HomeController> {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => ListTile(
+          title: Text("Nama Produk"),
+          subtitle: Text("Status : Baru"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Get.toNamed(Routes.PRODUCT),
+        child: Icon(
+          Icons.add,
         ),
       ),
     );
