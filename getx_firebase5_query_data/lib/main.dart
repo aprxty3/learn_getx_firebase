bf4da0ea795edb1b51854cfa0ccd1565473c0243
aprxty3 <ajiprxty3@gmail.com>
2022-03-25 10:45:30 +0700

    change navigator

--- getx_firebase5_query_data/lib/main.dart
+++ getx_firebase5_query_data/lib/main.dart
@@ -17,23 +17,31 @@ class MyApp extends StatelessWidget {
   final authC = Get.put(AuthController(), permanent: true);
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: authC.streamAuthStatus,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          print(snapshot.data);
-          return GetMaterialApp(
-            title: "Application",
-            initialRoute:
-                snapshot.data != null && snapshot.data!.emailVerified == true
-                    ? Routes.HOME
-                    : Routes.LOGIN,
-            getPages: AppPages.routes,
-            // home: snapshot.data != null ? HomeView() : LoginView(),
-          );
-        }
-        return LoadingView();
-      },
+    return GetMaterialApp(
+      title: "Application",
+      initialRoute: Routes.QUERY,
+      getPages: AppPages.routes,
+      // home: snapshot.data != null ? HomeView() : LoginView(),
     );
+
+    // return StreamBuilder<User?>(
+    //   stream: authC.streamAuthStatus,
+    //   builder: (context, snapshot) {
+    //     if (snapshot.connectionState == ConnectionState.active) {
+    //       print(snapshot.data);
+
+    //       return GetMaterialApp(
+    //         title: "Application",
+    //         initialRoute:
+    //             snapshot.data != null && snapshot.data!.emailVerified == true
+    //                 ? Routes.HOME
+    //                 : Routes.LOGIN,
+    //         getPages: AppPages.routes,
+    //         // home: snapshot.data != null ? HomeView() : LoginView(),
+    //       );
+    //     }
+    //     return LoadingView();
+    //   },
+    // );
   }
 }
