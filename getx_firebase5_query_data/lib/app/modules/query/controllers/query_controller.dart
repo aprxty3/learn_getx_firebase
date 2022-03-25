1527baaac8b5fa8659e9911d579ebfd610dc2473
aprxty3 <ajiprxty3@gmail.com>
2022-03-25 10:46:28 +0700

    create filter where feature

---
+++ getx_firebase5_query_data/lib/app/modules/query/controllers/query_controller.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class QueryController extends GetxController {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void filter(int age) async {
+    final result = await firestore
+        .collection("users")
+        .where('age', isGreaterThanOrEqualTo: age.toString())
+        .get();
+
+    if (result.docs.length > 0) {
+      print("Total data filter : ${result.docs.length}");
+      result.docs.forEach((element) {
+        var id = element.id;
+        var data = element.data();
+
+        print("ID : $id");
+        print("Data : $data");
+      });
+    } else {
+      print("tidak ada data filter : ${result.docs.length}");
+    }
+  }
+}
