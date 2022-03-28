e4d67e1eff3c7b4304ce1d997780009006c97640
aprxty3 <ajiprxty3@gmail.com>
2022-03-28 10:09:00 +0700

    implement firebase messaging topic

--- getx_firebase8_cloud_messaging/lib/app/modules/home/views/home_view.dart
+++ getx_firebase8_cloud_messaging/lib/app/modules/home/views/home_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
-class HomeView extends GetView<HomeController> {
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() async {
+    // subscribe to topic on each app start-up
+    await FirebaseMessaging.instance.subscribeToTopic('weather');
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
