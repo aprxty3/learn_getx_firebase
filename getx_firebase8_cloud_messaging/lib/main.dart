fac80535088a09f57753fd74f39fe9a172c1dee6
aprxty3 <ajiprxty3@gmail.com>
2022-03-28 10:33:42 +0700

    add initialized

--- getx_firebase8_cloud_messaging/lib/main.dart
+++ getx_firebase8_cloud_messaging/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -5,9 +6,16 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  // subscribe to topic on each app start-up
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   await Firebase.initializeApp();
 
+//   print("Handling a background message: ${message.messageId}");
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MyApp());
 }
 
