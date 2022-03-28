d8d9d63d9d06eeedb3d7fa06e0aa80baab617e65
aprxty3 <ajiprxty3@gmail.com>
2022-03-28 10:08:48 +0700

    extend widget

--- getx_firebase8_cloud_messaging/lib/main.dart
+++ getx_firebase8_cloud_messaging/lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
+  // subscribe to topic on each app start-up
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
